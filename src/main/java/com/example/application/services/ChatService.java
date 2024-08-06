096ee26be8fd807fc70deefc53becadf637c30f3
jhlee <jhlee@dbfis.co.kr>
2024-08-06 16:22:40 +0900

    Query Compression_Advanced RAG 일반 챗봇 구현 완료

--- src/main/java/com/example/application/services/ChatService.java
+++ src/main/java/com/example/application/services/ChatService.java
@@ -2,17 +2,40 @@ package com.example.application.services;
 
 import com.vaadin.flow.server.auth.AnonymousAllowed;
 import dev.hilla.BrowserCallable;
+import dev.langchain4j.data.document.Document;
+import dev.langchain4j.data.document.loader.FileSystemDocumentLoader;
+import dev.langchain4j.data.document.parser.TextDocumentParser;
+import dev.langchain4j.data.document.splitter.DocumentSplitters;
+import dev.langchain4j.memory.chat.MessageWindowChatMemory;
 import dev.langchain4j.memory.chat.TokenWindowChatMemory;
+import dev.langchain4j.model.Tokenizer;
+import dev.langchain4j.model.chat.ChatLanguageModel;
+import dev.langchain4j.model.embedding.EmbeddingModel;
 import dev.langchain4j.model.openai.OpenAiChatModel;
 import dev.langchain4j.model.openai.OpenAiStreamingChatModel;
 import dev.langchain4j.model.openai.OpenAiTokenizer;
+import dev.langchain4j.rag.DefaultRetrievalAugmentor;
+import dev.langchain4j.rag.RetrievalAugmentor;
+import dev.langchain4j.rag.content.retriever.ContentRetriever;
+import dev.langchain4j.rag.content.retriever.EmbeddingStoreContentRetriever;
+import dev.langchain4j.rag.query.transformer.CompressingQueryTransformer;
+import dev.langchain4j.rag.query.transformer.QueryTransformer;
 import dev.langchain4j.service.AiServices;
 import dev.langchain4j.service.TokenStream;
+import dev.langchain4j.store.embedding.EmbeddingStore;
+import dev.langchain4j.store.embedding.EmbeddingStoreIngestor;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ResourceLoader;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Sinks;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.PathMatcher;
+import java.util.List;
 
 @BrowserCallable
 @AnonymousAllowed
@@ -23,7 +46,16 @@ public class ChatService {
     private StreamingAssistant streamingAssistant;
 
     @Autowired
-    CustomerService customerService;
+    private ResourceLoader resourceLoader;
+
+    @Autowired
+    EmbeddingStore embeddingStore;
+
+    @Autowired
+    EmbeddingModel embeddingModel;
+
+    @Autowired
+    Tokenizer tokenizer;
 
     interface Assistant {
         String chat(String message);
@@ -34,7 +66,7 @@ public class ChatService {
     }
 
     @PostConstruct
-    public void init() {
+    public void init() throws IOException {
 
         if (OPENAI_API_KEY == null) {
             System.err.println("ERROR: OPENAI_API_KEY environment variable is not set. Please set it to your OpenAI API key.");
@@ -42,11 +74,62 @@ public class ChatService {
 
         var memory = TokenWindowChatMemory.withMaxTokens(2000, new OpenAiTokenizer("gpt-4o-mini"));
 
+
+        EmbeddingStoreIngestor ingestor = EmbeddingStoreIngestor.builder()
+                .documentSplitter(DocumentSplitters.recursive(300, 0))
+                .embeddingModel(embeddingModel)
+                .embeddingStore(embeddingStore)
+                .build();
+
+
+        PathMatcher pathMatcher = FileSystems.getDefault().getPathMatcher("glob:*.txt");
+        var resource = resourceLoader.getResource("classpath:documents");
+        File documentsDirectory = resource.getFile();
+
+        if (!documentsDirectory.isDirectory()) {
+            throw new IllegalArgumentException("'documents' is not a directory");
+        }
+
+        List<Document> documents = FileSystemDocumentLoader.loadDocuments(documentsDirectory.getPath(), pathMatcher, new TextDocumentParser());
+
+        var splitter = DocumentSplitters.recursive(100, 0, tokenizer);
+
+        //Document Ingestion
+        for (Document doc : documents){
+            ingestor.ingest(doc);
+        }
+
+        ChatLanguageModel chatLanguageModel = OpenAiChatModel.builder()
+                .apiKey(OPENAI_API_KEY)
+                .modelName("gpt-4o-mini")
+                .build();
+
+        QueryTransformer queryTransformer = new CompressingQueryTransformer(chatLanguageModel);
+
+        ContentRetriever contentRetriever = EmbeddingStoreContentRetriever.builder()
+                .embeddingStore(embeddingStore)
+                .embeddingModel(embeddingModel)
+                .maxResults(2)
+                .minScore(0.6)
+                .build();
+
+        RetrievalAugmentor retrievalAugmentor = DefaultRetrievalAugmentor.builder()
+                .queryTransformer(queryTransformer)
+                .contentRetriever(contentRetriever)
+                .build();
+
+        assistant = AiServices.builder(Assistant.class)
+                .chatLanguageModel(chatLanguageModel)
+                .retrievalAugmentor(retrievalAugmentor)
+                .chatMemory(MessageWindowChatMemory.withMaxMessages(10))
+                .build();
+
+        /*
         assistant = AiServices.builder(Assistant.class)
                 .chatLanguageModel(OpenAiChatModel.withApiKey(OPENAI_API_KEY))
                 .chatMemory(memory)
                 .build();
-
+        */
         streamingAssistant = AiServices.builder(StreamingAssistant.class)
                 .streamingChatLanguageModel(OpenAiStreamingChatModel.withApiKey(OPENAI_API_KEY))
                 .chatMemory(memory)
@@ -60,19 +143,18 @@ public class ChatService {
     public Flux<String> chatStream(String message) {
         Sinks.Many<String> sink = Sinks.many().unicast().onBackpressureBuffer();
 
-        /*
         streamingAssistant.chat(message)
                 .onNext(sink::tryEmitNext)
                 .onComplete(c -> sink.tryEmitComplete())
                 .onError(sink::tryEmitError)
                 .start();
-        */
+        /*
         customerService.chat(message)
                 .onNext(sink::tryEmitNext)
                 .onComplete(c -> sink.tryEmitComplete())
                 .onError(sink::tryEmitError)
                 .start();
-
+        */
         return sink.asFlux();
     }
 }
